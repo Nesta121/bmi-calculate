73689319c44c02b0de04729d644a8a37c38b8adb
Nesta <gfdiosg1456@gmail.com>
2025-04-24 11:52:21 +0700

    Create App.py

---
+++ App.py
@@ -0,0 +1,42 @@
+import streamlit as st
+st.title("BMI Calculation")
+st.markdown("---")
+
+kg=st.number_input("Weight(Kg.)",value=60.5,min_value=10.0,max_value=200.0,step=0.5)
+cm=st.number_input("Height(Cm.)",value=100,min_value=1,max_value=200,step=1)
+
+if st.button("Calculate"):
+    b=kg/(cm/100)**2
+    st.subheader(f"ค่าดัชนีของคุณคือ {b:.1f}")
+    
+    if b<18.5:
+        st.info("น้อยไป")
+        st.warning("เสี่ยงขาดสารอาหาร")
+        st.image("1.png")
+    elif b<23:
+        st.info("ปกติ")
+        st.success("มีโอกาสเกิดโรคแทรกซ้อนน้อยที่สุด")
+        st.image("2.png")
+    elif b<25:
+        st.info("เริ่มอ้วน")
+        st.warning("ภาวะน้ำหนักเกินมีโรคแทรกซ้อน")
+        st.image("3.png")
+    elif b<30:
+        st.info("อ้วน")
+        st.warning("มีโอกาสเกิดโรคสูง")
+        st.image("4.png")
+    else:
+        st.info("อ้วนมาก")
+        st.error("เสี่ยงต่อโรคร้าย")
+        st.image("5.png")
+    
+    
+
+
+
+
+
+    
+
+
+                
